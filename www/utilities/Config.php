0fa1115570c02134b8ed506130495a946aa45553
sgarabaya <santiago.garabaya@gmail.com>
2026-09-24 01:10:55 +0200

    Removed hardcoded creds

--- www/utilities/Config.php
+++ www/utilities/Config.php
@@ -1,23 +1,29 @@
 <?php
 abstract class Config
 {
+    private static function get_or(string $name, string $default = ""): string
+    {
+        $var = getenv($name, true);
+        return $var && !empty($var) ? $var : $default;
+    }
+
     public static function getDbHost(): string
     {
-        return "db";
+        return self::get_or("DB_HOST");
     }
     public static function getDbName(): string
     {
-        return "ruta9";
+        return self::get_or("DB_NAME");
     }
 
     public static function getDbUser(): string
     {
-        return "root";
+        return self::get_or("DB_USER");
     }
 
     public static function getDbPwd(): string
     {
-        return "root";
+        return self::get_or("DB_PWD");
     }
 }
 ?>
